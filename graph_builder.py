8aa0729a5ab072d3ab552361718fb153be6d67de
Jared Wight <JWight1@1800contacts.com>
2023-10-25 16:58:33 -0600

    main | WIP

--- graph_builder.py
+++ graph_builder.py
@@ -4,4 +4,4 @@ df = pd.read_excel("./WGUPS Distance Table.xlsx", header=7).drop("Unnamed: 1", a
 print(df.to_string())
 addresses = df["DISTANCE BETWEEN HUBS IN MILES"]
 print(addresses.to_string())
-df.replace()
+df.columns=[]
