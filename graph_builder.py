35d5bd59b1b4e1871fa4174b29e69fdd8e72569f
Jared Wight <JWight1@1800contacts.com>
2023-10-25 20:37:16 -0600

    main | WIP

--- graph_builder.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("./WGUPS Distance Table.xlsx", header=7).drop("Unnamed: 1", axis=1)
-print(df.to_string())
-addresses = df["DISTANCE BETWEEN HUBS IN MILES"]
-print(addresses.to_string())
-df.columns=[]
--- index.py
+++ index.py
@@ -1,3 +1,25 @@
 import delivery
 import truck
 
+import pandas as pd
+import networkx as nx
+
+df = pd.read_excel("./WGUPS Distance Table.xlsx", header=7).drop("Unnamed: 1", axis=1)
+addresses = []
+edges = {}
+
+for idx, address in enumerate(df.columns):
+    # skip label column
+    if idx == 0:
+        continue
+    i = idx-1
+    addresses.append({'id': i, 'address': address})
+    edges[i] = {}
+    for j in range(1, i+1):
+        weight = df.iat[i, j]
+        print([i, j, weight])
+        edges[i][j] = {'weight': weight}
+
+print(addresses)
+
+g = nx.Graph(edges)
